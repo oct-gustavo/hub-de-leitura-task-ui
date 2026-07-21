23629e4dfccd4b2f97f71383406b55cf19ccf6e4
oct-gustavo <dearaujoluisgustavo95@gmail.com>
2026-07-21 16:17:00 -0300

    Comandos para automação:Registro e Login

---
+++ cypress/support/commands.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker'
+
+Cypress.Commands.add('register', () => {
+
+    const fnome = faker.person.fullName()
+    const femail = faker.internet.email()
+    const ftelefone = faker.phone.number()
+    // Deve preencher o form de registro
+    cy.get('#name').type(fnome)
+    cy.get('#email').type(femail)
+    cy.get('#phone').type(ftelefone)
+    cy.get('#password').type("teste0210")
+    cy.get('#confirm-password').type("teste0210")
+    cy.get('#terms-agreement').click()
+    cy.get('#register-btn').click()
+    // Validação do Registro
+    cy.url().should('include', 'dashboard')
+
+})
+
+Cypress.Commands.add('login', () => {
+
+    cy.visit('login.html')
+    cy.get('#email').type('Neal_Heidenreich87@yahoo.com')
+    cy.get('#password').type('teste0210')
+    cy.get('#login-btn').click()
+    // Validação do Login
+    cy.url().should('include', 'dashboard')
+
+})
